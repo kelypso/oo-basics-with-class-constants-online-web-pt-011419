f1eca964248ce19fc06751399f1f89be5c4e47a6
Kelsey Low <kklowlow@gmail.com>
2019-02-22 23:51:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,19 +1,19 @@
-class Shoe
+class Shoe 
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+  
   BRANDS = []
   
   def initialize(brand)
     @brand = brand
-    if BRANDS.include?(brand)
+    if BRANDS.include?(brand) # checks if the brand is unique
     else
-      BRANDS << brand
+      BRANDS << brand # keeps track of unique brands - *why doesn't self work?
     end
   end
-
+  
   def cobble
-    self.condition = "new"
     puts "Your shoe is as good as new!"
+    self.condition = "new"
   end
 end
